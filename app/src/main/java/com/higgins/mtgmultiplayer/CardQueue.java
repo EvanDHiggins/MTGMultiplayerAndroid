a49d6cc3cd0b9cd3afbbe76c7190217df0a0b82c
EvanDHiggins <evan.higgins06@gmail.com>
2015-01-15 18:20:15 -0600

    Begun work on properly moving bitmap getter to async task. The previous implementation made the adapter in an async, but the actual bitmaps were still grabbed on the UI thread.

--- app/src/main/java/com/higgins/mtgmultiplayer/CardQueue.java
+++ app/src/main/java/com/higgins/mtgmultiplayer/CardQueue.java
@@ -5,11 +5,13 @@ import android.content.res.AssetManager;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.util.Log;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -29,6 +31,7 @@ public class CardQueue {
     AssetManager assetManager;
     ArrayList<String> cardNamesList;
     String folderPath;
+    Bitmap cardImage;
 
     public CardQueue(Context c, String folderName) {
 
@@ -56,18 +59,21 @@ public class CardQueue {
 
     public Bitmap getCardBitmap(int position) {
         Bitmap cardImage = null;
-        try {
-            InputStream inputS = assetManager.open(folderPath + File.separator +
-                    cardNamesList.get(position));
-            cardImage = BitmapFactory.decodeStream(inputS);
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "IOException");
-            e.printStackTrace();
-        } finally {
-            if (cardImage == null) {
-                Log.e(LOG_TAG, "Error, null cardImage");
-            }
-        }
+        BitmapGetterTask task = new BitmapGetterTask(assetManager, folderPath,
+                cardNamesList.get(position), cardImage);
+//        try {
+//            InputStream inputS = assetManager.open(folderPath + File.separator +
+//                    cardNamesList.get(position));
+//            cardImage = BitmapFactory.decodeStream(inputS);
+//        } catch (IOException e) {
+//            Log.e(LOG_TAG, "IOException");
+//            e.printStackTrace();
+//        } finally {
+//            if (cardImage == null) {
+//                Log.e(LOG_TAG, "Error, null cardImage");
+//            }
+//        }
+        task.execute();
         return cardImage;
     }
 
@@ -85,6 +91,53 @@ public class CardQueue {
         if(array.length > 0) {
             for(String file : array) {
                 Log.v(LOG_TAG, file);
+            }
+        }
+    }
+
+    private class BitmapGetterTask extends AsyncTask<Void, Void, Bitmap> {
+
+        AssetManager aManager;
+        String folderName;
+        String cardName;
+        WeakReference<Bitmap> cardImage;
+
+        public BitmapGetterTask(AssetManager assetManager, String folderPath, String cardName, Bitmap cardImage) {
+            aManager = assetManager;
+            if(aManager == null) {
+                Log.v(LOG_TAG, "AssetManager is null");
+            }
+            folderName = folderPath;
+            this.cardName = cardName;
+            this.cardImage = new WeakReference<Bitmap>(cardImage);
+        }
+
+        @Override
+        protected Bitmap doInBackground(Void... params) {
+            Bitmap cardImage = null;
+            try {
+                InputStream inputS = assetManager.open(folderPath + File.separator +
+                        cardName);
+                cardImage = BitmapFactory.decodeStream(inputS);
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "IOException");
+                e.printStackTrace();
+            } finally {
+                if (cardImage == null) {
+                    Log.e(LOG_TAG, "Error, null cardImage");
+                }
+            }
+            return cardImage;
+
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap cardImage) {
+            Bitmap cardBitmap = this.cardImage.get();
+            if(cardBitmap != null) {
+                cardBitmap = cardImage;
+            } else {
+                Log.e(LOG_TAG, "cardBitmap is null");
             }
         }
     }
